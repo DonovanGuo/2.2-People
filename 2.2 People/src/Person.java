6b47b5837c96436d4542895619835c4ac01e7673
BT_1N3_14 <BT_1N3_14@BT_1N3_14.bktech.local>
2017-11-21 09:12:33 -0500

    Steven did this

--- 2.2 People/src/Person.java
+++ 2.2 People/src/Person.java
@@ -1,4 +1,66 @@
 
-public abstract class Person {
+public abstract class Person
+{
+	private String FirstName;
+	private String FamilyName;
+	private String BirthDate;
+	private int height; // inches
+	
+	public Person(String FirstName, String FamilyName, String BirthDate, int height)
+	{
+		this.FirstName = FirstName;
+		this.FamilyName = FamilyName;
+		this.BirthDate = BirthDate;
+		this.height = height;
+	}
+
+	public String getFirstName() {
+		return FirstName;
+	}
+
+	public void setFirstName(String firstName) {
+		FirstName = firstName;
+	}
+
+	public String getFamilyName() {
+		return FamilyName;
+	}
+
+	public void setFamilyName(String familyName) {
+		FamilyName = familyName;
+	}
+
+	public String getBirthDate() {
+		return BirthDate;
+	}
+
+	public void setBirthDate(String birthDate) {
+		BirthDate = birthDate;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	public void setHeight(int height) {
+		this.height = height;
+	}
+
+	@Override
+	public String toString() {
+		return "Person First Name = " + getFirstName() + ", Family Name = " + getFamilyName() + ", Birth date = "
+				+ getBirthDate() + ", Height = " + getHeight() + ".";
+	}
+
+
+
+
+
+
+
+
+
+
 
 }
+	
