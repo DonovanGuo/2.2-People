1bd02661d6fd6b09cc1a52ca938e0bf25d7e129d
BT_1N3_14 <BT_1N3_14@BT_1N3_14.bktech.local>
2017-11-28 09:31:23 -0500

    STEVEN IS BAD

--- 2.2 People/src/Classroom.java
+++ 2.2 People/src/Classroom.java
@@ -1,17 +1,34 @@
 
 public class Classroom 
 {
-	private Person[] Student;
-	private Teacher Teacher;
-	public Classroom( Person[] Student , Teacher Teacher)
+	private Person[] student;
+	private Person teacher;
+	public Classroom( Person[] student , Person teacher)
 	{
-		this.Student = Student; 
-		this.Teacher = Teacher;
+		this.student = student; 
+		this.teacher = teacher;
 	}
 	public String getSubject()
 	{
-		return Teacher.getSubject();
+			return ((Teacher)teacher).getSubject();
 	}
 	
+	public double classAverage()
+	{
+		double ClassGpa = 0;
+		for(int i = 0; i < student.length; i++)
+		{
+			
+			ClassGpa = ClassGpa + ((Student)student[i]).getGPA();
+		}
+		return ClassGpa / student.length;
+	}
+	public String printClass()
+	{	 String totalClassstud = "";
+		for(int i =0; i < student.length; i++)
+		{
+			totalClassstud += "/n" (())  
+		}
+	}
 }
 
